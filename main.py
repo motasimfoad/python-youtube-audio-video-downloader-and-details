e1d7c87bd3169d4d1019c77823474b20d34dfb96
Motasim Foad <motasimfoad@gmail.com>
2021-04-18 01:13:41 -0400

    added youtube video information

--- main.py
+++ main.py
@@ -3,4 +3,17 @@ from pytube import YouTube
 
 # accepting input from user
 link = input("Enter YouTube video link: ")
+print("EG: https://www.youtube.com/watch?v=CDenUG1xMwg")
 yt = YouTube(link)
+
+# video title
+print("Title of the video: ", yt.title)
+
+# artist/author name
+print("Author name: ", yt.author)
+
+# video rating
+print("Video rating: ", yt.rating)
+
+# video views
+print("Video views: ", yt.views)
