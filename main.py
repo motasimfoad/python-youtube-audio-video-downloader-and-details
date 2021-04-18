402f86ebe440323429d11fbb1573cdcdbb3a0e93
Motasim Foad <motasimfoad@gmail.com>
2021-04-18 01:29:13 -0400

    video downloader with specific DIR

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 from pytube import YouTube
 
 # accepting input from user
-link = input("Enter YouTube video link: ")
 print("EG: https://www.youtube.com/watch?v=CDenUG1xMwg")
+link = input("Enter YouTube video link: ")
+print("You entered the link: ", link)
 yt = YouTube(link)
 
 # video title
@@ -17,3 +18,11 @@ print("Video rating: ", yt.rating)
 
 # video views
 print("Video views: ", yt.views)
+
+# getting high resolution video stream
+ys = yt.streams.get_highest_resolution()
+
+# download to your system in prefered location
+print("Download started ...")
+ys.download("./downloads")
+print("Download finished!")
